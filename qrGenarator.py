23eff4c4daf3794dd8d54122582b898c8e1ca97a
Ayramuh-ynah <humayra.hany@gmail.com>
2026-07-24 15:57:49 +0600

    colored qr code

--- qrGenarator.py
+++ qrGenarator.py
@@ -1,6 +1,18 @@
 import qrcode as qr
+from PIL import Image
+
 
 img = qr.make("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl")
 img.save("qrcode.png")
 
 
+#color qr code
+q = qr.QRCode( version=1, error_correction=qr.constants.ERROR_CORRECT_H, 
+box_size=10, border=4)
+
+data = input("Enter the data or link to generate QR code: ")
+
+q.add_data(data)
+q.make(fit=True)
+img = q.make_image(fill_color="red", back_color="white")
+img.save("colored_qrcode.png")
