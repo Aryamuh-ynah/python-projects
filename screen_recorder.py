829767ca65c95f933199fbf8554ad1e970d0c8a4
Ayramuh-ynah <humayra.hany@gmail.com>
2026-08-03 16:10:35 +0600

    [add]: game window

--- screen_recorder.py
+++ screen_recorder.py
@@ -38,7 +38,6 @@ import cv2
 import numpy as np
 import time
 from mss import mss
-
 # Screen size
 with mss() as sct:
     monitor = sct.monitors[1]  # Primary monitor
@@ -46,24 +45,18 @@ with mss() as sct:
     height = monitor["height"]
 
 dim = (width, height)
-
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 output = cv2.VideoWriter("/home/humayra/test.mp4", fourcc, 20.0, dim)
-
 duration = 10  # seconds
 end_time = time.time() + duration
-
 print("Recording started...")
-
 with mss() as sct:
     while time.time() < end_time:
         # Capture screen
-        img = sct.grab(monitor)
-        
+        img = sct.grab(monitor)       
         # Convert to numpy array (BGRA → BGR)
         frame = np.array(img)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)        
         output.write(frame)
 
 output.release()
---
+++ snake.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import time
+
+pygame.init()
+red = (255, 0, 0)
+blue = (0, 0, 200)
+grey = (200, 200, 200)
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+
+win_width = 600
+win_height = 400
+
+window = pygame.display.set_mode((win_width, win_height))
+time.sleep(5)
