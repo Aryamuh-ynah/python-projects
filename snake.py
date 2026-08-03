8bd3531081f4e4ab5d1ff1009deb454cf6d43420
Ayramuh-ynah <humayra.hany@gmail.com>
2026-08-03 16:14:45 +0600

    set all variables

--- snake.py
+++ snake.py
@@ -13,4 +13,17 @@ win_width = 600
 win_height = 400
 
 window = pygame.display.set_mode((win_width, win_height))
-time.sleep(5)
+# time.sleep(5)
+
+pygame.display.set_caption("Snake Game")
+
+sn = 10
+sn_speed = 15
+
+score = 0
+
+# fonts = pygame.font.get_fonts()
+
+# print(fonts) #print all available fonts
+font = pygame.font.Sysfont("comicsansms", 35)
+score_font = pygame.font.Sysfont("notosansmonocjksc", 25)
