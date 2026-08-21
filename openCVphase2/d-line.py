7ccad76e06811a2de2a3e1cfd46a43b797fad386
Ayramuh-ynah <humayra.hany@gmail.com>
2026-08-21 12:51:12 +0600

    Draw a line using open cv

---
+++ openCVphase2/d-line.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("phase1/test.png")
+
+if img is not None:
+
+    p1 = (100, 100)
+    p2 = (240, 40)
+    color = (225, 0, 0)  # Blue color in BGR
+    thikness = 2
+
+    cv2.line(img, p1, p2, color, thickness)
+
+    cv2.imshow("Image with Line", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
