4cf174449872b77436cd9ed3246c533d1d06883e
Ayramuh-ynah <humayra.hany@gmail.com>
2026-08-24 15:34:16 +0600

    [draw]: circle

--- openCVphase2/circle.py
+++ openCVphase2/circle.py
@@ -3,3 +3,9 @@ import cv2
 img = cv2.imread("phase1/test.png")
 
 if img is not None:
+    cv2.circle(img, (53,56), 44, (0, 255, 0), -1)  # Draw a filled green circle
+    cv2.imshow("Image with Circle", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Error: Image not found or unable to load.")
